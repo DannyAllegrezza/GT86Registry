ffed28e453f6ba72e11201d43d09f5c6585afbbe
danny allegrezza <danny@dannyallegrezza.com>
2017-10-24 12:17:27 -0400

    Recreate "Core" project

---
+++ src/GT86Registry.Core/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GT86Registry.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime ModifiedDate { get; set; }
+
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/GT86Registry.Core/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace GT86Registry.Core.Entities
+{
+    public class Car : BaseEntity
+    {
+        public string IdentityGuid { get; set; }
+        public bool IsManualTransmission { get; set; }
+        public string VIN { get; set; }
+
+        public Color Color { get; set; }
+        public int ColorId { get; set; }
+
+        public Year Year { get; set; }
+        public int YearId { get; set; }
+
+        public Manufactuer Manufactuer { get; set; }
+        public int ManufactuerId { get; set; }
+
+    }
+}
