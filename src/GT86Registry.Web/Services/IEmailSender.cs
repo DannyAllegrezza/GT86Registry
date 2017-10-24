732d30271177e4f8001393fa4a7ffcb0d2a06cec
danny allegrezza <danny@dannyallegrezza.com>
2017-10-24 12:21:13 -0400

    Recreate Web MVC project

---
+++ src/GT86Registry.Web/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GT86Registry.Web.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
