2dd22653dedb56642bf6e3a76aa00c6d49b9216f
danny allegrezza <danny@dannyallegrezza.com>
2017-11-28 17:51:36 -0500

    Update `ApplicationUser` entity and DbContext
    Added some new properties to the ApplicationUser, then modified the
    DbContext accordingly.

--- src/GT86Registry.Infrastructure/Identity/AppIdentityDbContext.cs
+++ src/GT86Registry.Infrastructure/Identity/AppIdentityDbContext.cs
@@ -12,6 +12,9 @@ namespace GT86Registry.Infrastructure.Identity
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>()
+                .Ignore(a => a.FullName);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
