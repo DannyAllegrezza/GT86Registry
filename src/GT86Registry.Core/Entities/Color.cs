2c3a7d241b4e5ef88f94b7d5e489f49e8d3be488
danny allegrezza <danny@dannyallegrezza.com>
2017-11-29 21:10:57 -0500

    Add constructor to `Color` class

--- src/GT86Registry.Core/Entities/Color.cs
+++ src/GT86Registry.Core/Entities/Color.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace GT86Registry.Core.Entities
 {
     public class Color : BaseEntity
-    { 
+    {
         public int Id { get; set; }
 
         public string Name { get; set; }
 
-        [MaxLength(3)]
         public string Code { get; set; }
+
+        #region Constructors
+
+        public Color(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        #endregion Constructors
+
+        #region Navigation Properties
 
         public List<ColorsModelYears> ModelColors { get; set; }
         public List<Vehicle> Vehicles { get; set; }
+
+        #endregion Navigation Properties
     }
 }
