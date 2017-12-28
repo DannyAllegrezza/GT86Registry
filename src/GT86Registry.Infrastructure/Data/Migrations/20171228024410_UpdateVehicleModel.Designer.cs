6b9c1ee00568e690bff7849bbd5de738b589f1a7
danny allegrezza <danny@dannyallegrezza.com>
2017-12-27 23:24:51 -0500

    Add migration
    Migration for previous commits changes.

--- src/GT86Registry.Infrastructure/Data/Migrations/20171228024410_UpdateVehicleModel.Designer.cs
+++ src/GT86Registry.Infrastructure/Data/Migrations/20171228024410_UpdateVehicleModel.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace GT86Registry.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(VehicleDbContext))]
-    partial class VehicleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171228024410_UpdateVehicleModel")]
+    partial class UpdateVehicleModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
