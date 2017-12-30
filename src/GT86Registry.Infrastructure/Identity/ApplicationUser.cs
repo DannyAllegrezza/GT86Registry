d68bbf6b3da7d53967194c1050105529e060f6c3
danny allegrezza <danny@dannyallegrezza.com>
2017-12-29 23:00:48 -0500

    Update ApplicationUser properties and migration

--- src/GT86Registry.Infrastructure/Identity/ApplicationUser.cs
+++ src/GT86Registry.Infrastructure/Identity/ApplicationUser.cs
@@ -1,26 +1,28 @@
-﻿using GT86Registry.Core.Interfaces;
+﻿using GT86Registry.Core.Entities;
+using GT86Registry.Core.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace GT86Registry.Infrastructure.Identity
 {
     public class ApplicationUser : IdentityUser, ISocialMediaLinks
     {
+        public ApplicationUser()
+        {
+            CreatedDate = DateTimeOffset.UtcNow;
+        }
+
         // Extend the existing Identity user information here
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-
-        public string InstagramUri { get; set; }
-
-        public string FacebookUri { get; set; }
 
         public string City { get; set; }
 
-        public string State { get; set; }
-
-        public string PostalCode { get; set; }
-
         public string Country { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+
+        public string FacebookUri { get; set; }
+
+        public string FirstName { get; set; }
 
         public string FullName
         {
@@ -38,5 +40,17 @@ namespace GT86Registry.Infrastructure.Identity
                 return fullName;
             }
         }
+
+        public string InstagramUri { get; set; }
+
+        public DateTimeOffset LastActivityDate { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTimeOffset ModifiedDate { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string State { get; set; }
     }
 }
---
+++ src/GT86Registry.Infrastructure/SeedData.cs
@@ -0,0 +1,12 @@
+﻿namespace GT86Registry.Infrastructure
+{
+    public class SeedData
+    {
+        public const string INSTAGRAM_URI = "https://www.instagram.com/dude_danny/";
+        public const string TOYOTA_INSTAGRAM_URI = "https://www.instagram.com/toyotausa/";
+        public const string SUBARU_INSTAGRAM_URI = "https://www.instagram.com/subaru_usa/";
+
+        public const string TOYOTA_FACEBOOK_URI = "https://www.facebook.com/toyota/";
+        public const string SUBARU_FACEBOOK_URI = "https://www.facebook.com/subaruofamerica/";
+    }
+}
