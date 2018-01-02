7a6e0f6f318c935ce6f0f0fc440a49aa33691725
danny allegrezza <danny@dannyallegrezza.com>
2018-01-01 22:54:16 -0500

    Add profile photo to users accounts

---
+++ src/GT86Registry.Infrastructure/Identity/Migrations/20180102034930_AddProfileImageUri.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GT86Registry.Infrastructure.Identity.Migrations
+{
+    public partial class AddProfileImageUri : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePhotoUri",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePhotoUri",
+                table: "AspNetUsers");
+        }
+    }
+}
