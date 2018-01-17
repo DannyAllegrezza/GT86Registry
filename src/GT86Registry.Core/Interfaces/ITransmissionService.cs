f36a574e7f6f6f97651e2f41797afb973da3e0c6
danny allegrezza <danny@dannyallegrezza.com>
2018-01-16 23:54:08 -0500

    Create Transmission service interface

---
+++ src/GT86Registry.Core/Interfaces/ITransmissionService.cs
@@ -0,0 +1,11 @@
+﻿using GT86Registry.Core.Entities;
+using System.Collections.Generic;
+
+namespace GT86Registry.Core.Interfaces
+{
+    public interface  ITransmissionService
+    {
+        IEnumerable<Transmission> GetTransmissionOptionsForModel(int year, string model);
+        IEnumerable<Transmission> GetTransmissionOptionsForModel(Model model);
+    }
+}
