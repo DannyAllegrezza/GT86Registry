c3fac2d187025e6d7da5df98224d9926da40f84f
danny allegrezza <danny@dannyallegrezza.com>
2018-02-04 19:32:36 -0500

    Update migrations
    Add the new HexValue migration. This prop will be used to store the CSS
    hex value of the color.

--- src/GT86Registry.Infrastructure/Data/Migrations/20180205002645_AddColorHexValue.Designer.cs
+++ src/GT86Registry.Infrastructure/Data/Migrations/20180205002645_AddColorHexValue.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace GT86Registry.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(VehicleDbContext))]
-    partial class VehicleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180205002645_AddColorHexValue")]
+    partial class AddColorHexValue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/GT86Registry.Infrastructure/Data/Migrations/20180205002645_AddColorHexValue.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GT86Registry.Infrastructure.Data.Migrations
+{
+    public partial class AddColorHexValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HexValue",
+                table: "Color",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HexValue",
+                table: "Color");
+        }
+    }
+}
