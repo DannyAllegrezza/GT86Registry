c51b56e5b3d32be70d3622d924c519f8ac442540
danny allegrezza <danny@dannyallegrezza.com>
2018-02-06 12:08:49 -0500

    Stub out additional functions

--- src/GT86Registry.Web/Controllers/GarageController.cs
+++ src/GT86Registry.Web/Controllers/GarageController.cs
@@ -1,32 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using GT86Registry.Infrastructure.Data;
 using GT86Registry.Infrastructure.Identity;
-using GT86Registry.Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GT86Registry.Web.Controllers
 {
     [Authorize]
     public class GarageController : Controller
     {
-        private readonly VehicleRepository _vehicleRepository;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly VehicleRepository _vehicleRepository;
 
         public GarageController(VehicleRepository vehicleRepository, UserManager<ApplicationUser> userManager)
         {
             _vehicleRepository = vehicleRepository;
             _userManager = userManager;
         }
+
         public IActionResult Index()
         {
             var user = _userManager.GetUserAsync(User).Result;
             var vehicles = _vehicleRepository.GetVehiclesByUserId(user.Id);
 
             return View(vehicles);
+        }
+
+        public IActionResult AddVehicle()
+        {
+            return View();
+        }
+
+        public IActionResult EditVehicle()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteVehicle()
+        {
+            return View();
         }
     }
 }
